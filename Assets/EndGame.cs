2c17cb24b9a3da81073e81ba257e9d2b5664d721
Peter Tieu <ptieu@ucsc.edu>
2019-02-18 22:24:32 -0800

    Lots of stuff
    Fleshed out the level

---
+++ Assets/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour
+{
+  public Player Keys;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+      if(Keys.collected == 10)
+      {
+        MainMenuScene("TitleScreen");
+      }
+
+    }
+
+    public void MainMenuScene(string SceneName)
+    {
+      SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float gravity = 15.0f;
+    public float jumpSpeed = 8.0f;
+    public float rotationY;
+    public float rotationX;
+
+    public bool enter = true;
+
+
+    private Vector3 rotateValue;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    public Text collectedText;
+    public int collected = 0;
+
+    public bool OpenDoor1 = false;
+    public bool OpenDoor2 = false;
+
+    Animator m_Animator;
+
+
+    void Start()
+    {
+      controller = GetComponent<CharacterController>();
+
+
+      gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded) // Only allows player to jump when touching the ground
+        {
+          moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+          moveDirection = transform.TransformDirection(moveDirection);
+          moveDirection = moveDirection * speed;
+
+          if (Input.GetButton("Jump"))
+         {
+             moveDirection.y = jumpSpeed;
+         }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // Rotate the view
+        rotationY = Input.GetAxis("Mouse X");
+        rotationX = Input.GetAxis("Mouse Y");
+        rotateValue = new Vector3(rotationX, rotationY * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+
+        collectedText.text = "Keys collected: " + collected.ToString ();
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.gameObject.CompareTag("Collectable")) // adds to collected number and destroys key
+      {
+        if(enter)
+        {
+          collected += 1;
+          Destroy(other.transform.root.gameObject);
+        }
+      }
+
+      if(other.gameObject.CompareTag("Door1")) // Opens the door if 3 or more keys are collected
+        {
+          m_Animator = other.transform.root.gameObject.GetComponent<Animator>();
+          if(collected >= 3)    {  m_Animator.SetBool("OpenDoor1", true);}
+
+
+
+        }
+      if(other.gameObject.CompareTag("Door2")) // Opens the door if 5 or more keys are collected
+          {
+            m_Animator = other.transform.root.gameObject.GetComponent<Animator>();
+            if(collected >= 5)    {  m_Animator.SetBool("OpenDoor2", true);}
+
+
+
+          }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+
+    }
+
+}
